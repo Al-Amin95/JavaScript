97deffd81aafb904b9aa358b99d1544bd1c5f0d7
Al-Amin95 <alamin28.sylhet@gmail.com>
2024-08-20 20:30:54 +0600

    JavaScript

--- Javascript basic-advance/1. Introduction/practice.js
+++ Javascript basic-advance/1. Introduction/practice.js
@@ -1,27 +1,39 @@
-// change content 
-function contet_change(){ 
-document.getElementById("content_demo").innerHTML="Content:  Learn JavaScript";
+// function content(){
+//     let con=document.getElementById("demo_content");
+//     con.innerHTML="Change content: Learn javaScript";
+// }
+
+function attribute(){ 
+    let att=document.getElementById("demo_attribute");
+    att.style="color: red; font-size: 30px; font-weight: 800";
 }
 
-function on(){ 
-document.getElementById("img_demo").src="/Javascript basic-advance/image/on.gif";
+
+//change attribute
+function ON(){ 
+    let att=document.getElementById("demo_img");
+    att.src="/Javascript basic-advance/00. image/on.gif";
 }
-function off(){ 
-document.getElementById("img_demo").src="/Javascript basic-advance/image/off.gif";
+function OFF(){ 
+    let att=document.getElementById("demo_img");
+    att.src="/Javascript basic-advance/00. image/off.gif";
+   
 }
 
-function change_style(){ 
-    let ele=document.getElementById("style_demo");
-    ele.style.color="red";
-    ele.style.fontSize="20px";
-    ele.style.fontWeight="Bold";
+
+//change attriubte style 
+function attribute(){ 
+    let att=document.getElementById("demo_style");
+    att.style="color: red; font-size: 30px; font-weight: 800";
 }
 
-function  hide(){ 
-    let el=document.getElementById("hide_show");
-    el.style.display="none";
-}
-function  show(){ 
-    let el=document.getElementById("hide_show");
-    el.style.display="block";
-}
+// change : show and hide  content of html tag 
+// function hide(){ 
+//     let att=document.getElementById("demo_sw");
+//     att.style.display="none";
+// }
+
+// function show(){ 
+//     let att=document.getElementById("demo_sw");
+//     att.style.display="block";
+// }
---
+++ Javascript basic-advance/2. where to write/index.js
@@ -0,0 +1,18 @@
+
+//change attriubte style 
+function attribute(){ 
+    let att=document.getElementById("demo_style");
+    att.style="color: red; font-size: 30px; font-weight: 800";
+}
+
+// change : show and hide  content of html tag 
+function hide(){ 
+    let att=document.getElementById("demo_sw");
+    att.style.display="none";
+}
+
+function show(){ 
+    let att=document.getElementById("demo_sw");
+    att.style.display="block";
+}
+
